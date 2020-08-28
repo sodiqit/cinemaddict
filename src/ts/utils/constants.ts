76e220994a867319962c2e345e527ef6ed1ee0e9
Daniil <daniil.petro3@gmail.com>
2020-08-28 18:02:52 +0500

    add classes in constants

--- src/ts/utils/constants.ts
+++ src/ts/utils/constants.ts
@@ -1,3 +1,14 @@
 export const constants = {
-  ACTIVE_CLASS: 'film-card__controls-item--active',
+  CLASSES: {
+    ACTIVE_CLASS: 'film-card__controls-item--active',
+    FILMS_CONTAINER: 'films-list__container',
+    FILMS_EXTRA: 'films-list--extra',
+    SHOW_BUTTON: 'films-list__show-more',
+    SORT: 'sort',
+    SORT_ACTIVE: 'sort__button--active',
+    FILTERS: 'main-navigation__items',
+    FILTER_ACTIVE: 'main-navigation__item--active',
+    NAVIGATION_CONTAINER: 'main-navigation',
+    PROFILE_RATING: 'profile__rating',
+  },
 };
