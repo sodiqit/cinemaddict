9491aabc623e3518cf92afb6586ce88267ab7969
Daniil <daniil.petro3@gmail.com>
2020-09-01 03:10:51 +0500

    create basic class for film popup

---
+++ src/ts/components/film-popup.ts
@@ -0,0 +1,16 @@
+type FilmInfoPopup = {
+
+};
+
+class FilmPopup {
+  private filmsInfo: FilmInfoPopup;
+
+  constructor(filmsInfo: FilmInfoPopup) {
+    this.filmsInfo = filmsInfo;
+  }
+}
+
+export {
+  FilmInfoPopup,
+  FilmPopup,
+};
